ac90c5b4e00c700d8879d1193fdfaa3a97f91e7d
akeyser2 <akeyser2@zagmail.gonzaga.edu>
2022-09-06 18:23:19 -0700

    addprint

---
+++ PythonBasicsFun/main.py
@@ -0,0 +1,47 @@
+#this is how you comment, ignored by python just like C++
+
+"""
+This is how you make a block coment
+just like /* in C++
+see, it still keeps going
+forever
+"""
+
+#game plan:
+#1. variables
+#2. operators
+#3. get input from user
+#4. formatting decimal nums
+
+
+#VARIABLES:
+#stores a value
+x = 5 #x HOLDS 5, not x equals 5
+#the value 5 is an int
+print(x)
+#we can check the data type of x
+print(type(x))
+#we can reassign x
+x = 5.5
+#this is a float datatype (has a fractional part)
+print(x)
+print(type(x))
+#we can also use strings
+x = "heyo"
+print(x)
+print(type(x))
+
+
+#OPERATORS
+#python follows PEMDAS (and more)
+# +, -, 
+# * is multiplication
+print(4 * 5)
+# / is floating point division (like normal division)
+print(5 / 2)
+# // is integer devision (the whole number version of /)
+print(5 // 2)
+# % is the modulus operator (gives remainder of integer division)
+print(5 % 2)
+# ** is the exponent operator (power)
+print(5 ** 2)
